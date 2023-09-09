77883470f81ac4cdba67f813e061ad654828fa5a
abord77 <raymond2003li@gmail.com>
2023-09-09 16:02:29 -0400

    Added animated background and login page

---
+++ todo-list/date.js
@@ -0,0 +1,29 @@
+//jshint esversion:6
+
+export const getDate = () => {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+};
+
+/*
+export default function getDay() {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+};*/
+
--- todo-list/index.js
+++ todo-list/index.js
@@ -2,43 +2,72 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { getDate } from "./date.js";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-let taskListPersonal = [];
-let taskListWork = [];
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const taskListPersonal = [];
+const taskListWork = [];
+
 app.get("/", (req, res) => {
-  res.render(__dirname + "/index.ejs", {
+  const day = getDate();
+
+  res.render(__dirname + "/views/login.ejs", {
+    listTitle: day,
     tasksPersonal: taskListPersonal,
-    tasksWork: taskListWork,
   });
 });
 
-app.post("/submit-personal", (req, res) => {
+app.get("/personal", (req, res) => {
+  const day = getDate();
+
+  res.render(__dirname + "/views/list-personal.ejs", {
+    listTitle: day,
+    tasksPersonal: taskListPersonal,
+  });
+});
+
+app.get("/work", function(req, res){
+  res.render(__dirname + "/views/list-work.ejs", {
+    listTitle: "Work List", 
+    tasksWork: taskListWork
+  });
+});
+
+app.post("/", (req, res) => {
+  const usr = req.body["uname"];
+  const pwd = req.body["psw"];
+  res.redirect("/personal");
+});
+
+app.post("/personal", (req, res) => {
   const taskName = req.body["taskName"];
   if (taskName !== "") {
     taskListPersonal.push(taskName);
   }
-  res.redirect("/");
+  res.redirect("/personal");
 });
 
-app.post("/submit-work", (req, res) => {
+app.post("/work", (req, res) => {
   const taskName = req.body["taskName"];
   if (taskName !== "") {
     taskListWork.push(taskName);
   }
-  res.redirect("/");
+  res.redirect("/work");
 });
 
+/*
 app.post("/removetask", (req, res) => {
   console.log("hi");
   res.redirect("/");
-});
+});*/
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
